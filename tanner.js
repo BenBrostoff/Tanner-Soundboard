369d022487466359a4f085cefd86cb5eb96fc66e
Ben Brostoff & Greg <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-14 13:06:55 -0400

    Set up completed - waiting on Brendan to record

--- tanner.js
+++ tanner.js
@@ -1,8 +1,48 @@
 $(document).ready(function() {
 
   $("#test").click(function() {
-    console.log("FUCK");
-    document.getElementById('sound_1').play();
+    console.log("BOX 1");
+    document.getElementById('back_to_work').play();
+  });
+
+  $("#test_2").click(function() {
+    console.log("BOX 2");
+    document.getElementById('timebox').play();
+  });
+
+  $("#test_3").click(function() {
+    console.log("BOX 3");
+    document.getElementById('thread').play();
+  });
+
+  $("#test_4").click(function() {
+    console.log("BOX 4");
+    document.getElementById('question').play();
+  });
+
+  $("#test_5").click(function() {
+    console.log("BOX 5");
+    document.getElementById('punt').play();
+  });
+
+  $("#test_6").click(function() {
+    console.log("BOX 6");
+    document.getElementById('inappropriate').play();
+  });
+
+  $("#test_7").click(function() {
+    console.log("BOX 7");
+    document.getElementById('integrity').play();
+  });
+
+  $("#test_8").click(function() {
+    console.log("BOX 8");
+    document.getElementById('feedback').play();
+  });
+
+  $("#test_9").click(function() {
+    console.log("BOX 9");
+    document.getElementById('read').play();
   });
 
 });
